4c02fa1d2a2c29d4f98cb85dc677b0132eb378d7
mopuppet <mopuppet>
2023-04-20 13:35:19 +1000

    added CSS

--- getBadges.py
+++ getBadges.py
@@ -1,29 +1,50 @@
 import requests
 
-# Get user X's badges, get titles for dictionary comparison
-r = requests.get("https://tryhackme.com/api/badges/get/supadupalipa")
+# Change this to your username
+username = "JohnHammond"
+
+# Get user X's badges
+r = requests.get("https://tryhackme.com/api/badges/get/" + username)
 response = r.json()
+
+# Create list of badge names to compare to ALL badges availiable
 userBadges = []
 for i in response:
     userBadges.append(i['name'])
 
-# Use below to create an dictionary of titles to images
+# Get all possible badges
 r = requests.get("https://tryhackme.com/api/badges/get")
 response = r.json()
+
+'''
+Create two dictionaries, both with badge name as the key.
+allBadges value will be the image location for the badge
+badgeText value will be the description of the badge
+'''
 allBadges = {}
+badgeText = {}
 for i in response:
     allBadges.update({ i['name'] : "https://tryhackme.com" + i['image']})
+    badgeText.update( {i['name'] : i['description']})
 
+# Just basic HTML and some CSS to make the output readable
 outputHTML = ""
+outputHTML += "<!DOCTYPE html>\n"
+outputHTML += "<html>\n"
+outputHTML += '<style> .badges\n {\ndisplay: grid;\ngrid-template-columns: repeat(4, 1fr);\ngrid-gap: 20px;\nmargin-top: 20px;\n}\n'
+outputHTML += 'img\n {\nwidth: 100px;\n}\n'
+outputHTML += '.title\n {\nfont-weight: bold;\n}\n</style>\n'
+outputHTML += '<div class="badges"> \n'
+
 with open("badges.html", "w") as outputFile:
-    outputHTML += "<!DOCTYPE html>\n"
-    outputHTML += "<html>\n"
-    outputHTML += '<style> .badges\n {\ndisplay: grid;\ngrid-template-columns: repeat(4, 1fr);\ngrid-gap: 20px;\n}\n</style>\n'
-    outputHTML += '<div class="badges"> \n'
     for i in userBadges:
+        # If a user has one of the possible badges, add the info to the html doc
         if i in allBadges:
-            outputHTML += "<h3>" + i + "</h3>"
+            outputHTML += '<div>'
             outputHTML += '<img src=' + '"' + allBadges[i] + '">'
+            outputHTML += '<p class="title">' + i + '</p>'
+            outputHTML += "<p>" + badgeText[i] + "</p>"
+            outputHTML += '</div>'
             outputHTML += "\n" 
     
     outputHTML += '<div>'
